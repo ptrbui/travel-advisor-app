0914a1937687bed158d08969f42edd2d3cf007c3
ptrbui <peterbui@usc.edu>
2024-03-30 18:18:34 -0700

    map locations

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const [places, setPlaces] = useState([]);
 
     const [coordinates, setCoordinates] = useState({});
-    const [bounds, setBounds] = useState(null);
+    const [bounds, setBounds] = useState({});
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude} }) => {
@@ -19,14 +19,12 @@ const App = () => {
     }, []);
 
     useEffect(() => {
-        if (bounds !== null) {
             getPlacesData(bounds.sw, bounds.ne)
                 .then((data) => {
                     console.log(data)
 
                     setPlaces(data);
                 })
-        }
     }, [coordinates, bounds]);
 
     return (
@@ -35,13 +33,16 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md = {4}>
-                    <List />
+                    <List
+                        places = {places}
+                    />
                 </Grid>
                 <Grid item xs={12} md = {8}>
                     <Map
                         setCoordinates = {setCoordinates}
                         setBounds = {setBounds}
                         coordinates = {coordinates}
+                        places = {places}
                     />
                 </Grid>
             </Grid>
